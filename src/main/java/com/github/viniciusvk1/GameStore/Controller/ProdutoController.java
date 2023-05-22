3984392267fecdb29346497c682a0e2549d0b487
Vinicius <viniciusalmeida.vk1@gmail.com>
2023-05-22 15:29:56 -0300

    methods crud implements in model Produto

--- src/main/java/com/github/viniciusvk1/GameStore/Controller/ProdutoController.java
+++ src/main/java/com/github/viniciusvk1/GameStore/Controller/ProdutoController.java
@@ -1,11 +1,69 @@
 package com.github.viniciusvk1.GameStore.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.viniciusvk1.GameStore.model.Produto;
+import com.github.viniciusvk1.GameStore.repository.ProdutoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/produtos")
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    // Metodo para lista todos os produtos ja cadastrados
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAll() {
+        return ResponseEntity.ok(produtoRepository.findAll());
+    }
+
+    // Metodo para listar um produto buscando pelo id ja definido
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id) {
+        return produtoRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // Metodo para buscar um produto pelo nome
+    @GetMapping("/buscar/{nome}")
+    public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
+        return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+    }
+
+    // Metodo para adicionar um novo produto
+    @PostMapping("/adicionar")
+    public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+    }
+
+    // Metodo para editar um produto ja existente
+    @PutMapping("/editar")
+    public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
+        return produtoRepository.findById(produto.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // Metodo para deletar um produto ja existente
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/deletar/{id}")
+    public void delete(@PathVariable Long id) {
+        Optional<Produto> categoria = produtoRepository.findById(id);
+
+        if (categoria.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        produtoRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/github/viniciusvk1/GameStore/repository/ProdutoRepository.java
+++ src/main/java/com/github/viniciusvk1/GameStore/repository/ProdutoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ProdutoRepository extends JpaRepository {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
