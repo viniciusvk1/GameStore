09092d54a16a6927250456ea54771aaf6e5cee24
Vinicius <viniciusalmeida.vk1@gmail.com>
2023-05-29 12:02:43 -0300

    adicionando security

--- src/main/java/com/github/viniciusvk1/GameStore/model/Produto.java
+++ src/main/java/com/github/viniciusvk1/GameStore/model/Produto.java
@@ -1,9 +1,21 @@
 package com.github.viniciusvk1.GameStore.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,21 +25,36 @@ import lombok.Setter;
 @Table(name = "tb_produtos")
 public class Produto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotBlank
-    private String nome;
+	@NotNull(message = "Nome é obrigatório!")
+	private String nome;
 
-    @NotBlank
-    private String descricao;
+	@Size(max = 500)
+	private String descricao;
 
-    @NotNull
-    private Double preco;
+	@NotNull(message = "Console é obrigatório!")
+	private String console;
 
-    @ManyToOne
-    @JsonIgnoreProperties("produtos")
-    private Categoria categoria;
+	private int quantidade;
+
+	@Column(name = "data_lancamento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataLancamento;
+
+	@NotNull(message = "Preço é obrigatório!")
+	@Positive(message = "O preço deve ser maior do que zero!")
+	private BigDecimal preco;
+
+	private String foto;
+
+	@Column(columnDefinition = "integer default 0")
+	private int curtir;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 
 }
